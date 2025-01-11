a85c577b48e41d79a90bebd8d2720d6cddd9914e
Sarthak Rasal <varshanr75@gmail.com>
2025-01-11 21:58:31 +0530

    added new cpp files and half of a pattern code

---
+++ Learning More/PR3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tealeaves = 50;
+    int waterTemperture = 85.5;
+    double priceOfTea = 299.99;
+    char teaGrade = 'A';
+    bool isTeaReady = false; // 0
+
+    return 0;
+}
---
+++ Learning More/PR4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    unsigned smallTeaPack = 100; // Tea packs cannot be negative
+
+    long largeTeaStorage = 10000000000;
+    return 0;
+}
