845565c8ee1e2938b05f0b5ce0367f4e371eabb5
Sarthak Rasal <varshanr75@gmail.com>
2025-02-11 17:11:08 +0530

    2d array

--- 45 2D arrays/1_2Darr.cpp
+++ 45 2D arrays/1_2Darr.cpp
@@ -5,4 +5,49 @@ using namespace std;
 
 int main()
 {
+    // declaration
+    int arr[3][3];
+
+    // initialisation
+    int brr[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    // access
+    cout << brr[2][2] << endl;
+
+    cout << endl;
+
+    // printing row-wise
+    // outer loop
+    cout << "Row-wise printed array is :";
+    cout << endl;
+    for (int i = 0; i < 3; i++)
+    {
+
+        // inner loop
+        for (int j = 0; j < 3; j++)
+        {
+            cout << brr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    // printing column-wise
+    // outer loop
+    cout << "Column-wise printed array is :";
+    cout << endl;
+    for (int i = 0; i < 3; i++)
+    {
+
+        // inner loop
+        for (int j = 0; j < 3; j++)
+        {
+            // only i and j are interchanged here
+            cout << brr[j][i] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
