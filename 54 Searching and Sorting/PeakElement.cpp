6479ffc76b3a4eb014c9372f276c93638aea1288
Sarthak Rasal <varshanr75@gmail.com>
2025-03-09 14:10:08 +0530

    peak element

---
+++ 54 Searching and Sorting/PeakElement.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findPeakIndex(vector<int> arr)
+{
+    int s = 0;
+    int e = arr.size() - 1;
+    int mid = s + (e - s) / 2;
+
+    while (s < e)
+    {
+        if (arr[mid] < arr[mid + 1])
+        {
+            // right search
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+int peakIndexInMountainArray(vector<int> &arr)
+{
+    return findPeakIndex(arr);
+}
